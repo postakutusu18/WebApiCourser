2229ee575c71cb428ef57e4e474f2698f4344c53
postakutusu18 <postakutusu18@hotmail.com>
2022-05-07 19:41:11 +0300

    repository

---
+++ Contracts/IProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+
+namespace Contracts
+{
+    public interface IProjectRepository
+    {
+        IEnumerable<Project> GetAllProjects(bool trackChanges);
+        Project GetProject(Guid id, bool trackChanges);
+        void CreateProject(Project project);
+        void DeleteProject(Project project);
+
+    }
+}
---
+++ Repository/ProjectRepository.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository
+{
+    public class ProjectRepository : RepositoryBase<Project>, IProjectRepository
+    {
+        public ProjectRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public void CreateProject(Project project) =>Create(project);
+
+        public void DeleteProject(Project project)=>Delete(project);
+
+        public IEnumerable<Project> GetAllProjects(bool trackChanges)=>
+            FindAll(trackChanges)
+            .OrderBy(p=>p.Name)
+            .ToList();
+
+        public Project GetProject(Guid id, bool trackChanges) =>
+            FindByCondition(p => p.Id.Equals(id), trackChanges)
+            .SingleOrDefault();
+    }
+}
